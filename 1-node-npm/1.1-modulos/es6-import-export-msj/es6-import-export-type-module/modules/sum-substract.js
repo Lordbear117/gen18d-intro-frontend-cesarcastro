db8230ab396076fa45393fec4391f04232437cc2
Cesar.castrop <cesar.castrop@coppel.com>
2022-10-25 19:22:42 -0600

    Correccion de carpetas

--- 1-node-npm/1.1-modulos/es6-import-export-msj/es6-import-export-type-module/modules/sum-substract.js
+++
@@ -1,9 +0,0 @@
-function sum(a, b) {
-    return a + b;
-}
-
-function substract(a, b) {
-    return a - b;
-}
-
-export { sum, substract };
--- assets/readme.md
+++
@@ -1,1 +0,0 @@
-https://drive.google.com/drive/u/0/folders/1ioW9pgR71OIJTiLDrXqNv3XgPv9IulhQ
